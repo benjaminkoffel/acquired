90623841c236fc2abc64cae6b751d554507113eb
debian <benjaminkoffel@gmail.com>
2018-12-08 18:53:58 +1100

    fix install script

--- agent/install.sh
+++ agent/install.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
-wget -O /usr/bin/local/ https://github.com/google/rekall/releases/download/v1.5.1/linpmem-2.1.post4
-chmod 700 /usr/bin/local/linpmem-2.1.post4
-cp -f acquired.py /usr/bin/local/
-chmod 700 /usr/bin/local/acquired.py
-cp -f acquired.service /etc/systemd/system/
+
+wget -O /usr/local/sbin/linpmem-2.1.post4 https://github.com/google/rekall/releases/download/v1.5.1/linpmem-2.1.post4
+chmod 700 /usr/local/sbin/linpmem-2.1.post4
+
+cp -f agent/acquired.py /usr/local/sbin
+chmod 700 /usr/local/sbin/acquired.py
+
+cp -f agent/acquired.service /etc/systemd/system/
 chmod 700 /etc/systemd/system/acquired.service
