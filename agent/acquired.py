b3002ac85902545c5aef4e25c15b01d552b853e8
Benjamin Koffel <benjaminkoffel@gmail.com>
2018-12-08 20:32:14 +1100

    update agent to use ec2 auth

--- agent/acquired.py
+++ agent/acquired.py
@@ -10,54 +10,75 @@ import sys
 import threading
 import time
 import urllib.request
+import base64
 
 logging.basicConfig(level=logging.DEBUG, format='time="%(asctime)s" level=%(levelname)s %(message)s', stream=sys.stdout)
 
-path = '/etc/acquired'
+path = '/usr/local/etc/acquired'
 
-def http_enrol(url, key, account, instance):
-    headers = {'Authorization': f'Bearer {key}'}
-    request = urllib.request.Request(f'{url}/enrol/{account}/{instance}', headers=headers)
+def http_get(url, status=200, headers=None):
+    request = urllib.request.Request(url=url, headers=headers)
     with urllib.request.urlopen(request) as r:
-        status = r.getcode()
-        if status != 200:
-            raise Exception(f'enrol returned unexpected status code {status}')
-        return json.loads(r.read())
+        if r.getcode() != status:
+            raise Exception('GET {} returned unexpected status code {}.'.format(url, r.getcode()))
+        return r.read()
+
+def http_document():
+    return \
+'''{
+  "devpayProductCodes" : null,
+  "marketplaceProductCodes" : null,
+  "privateIp" : "10.0.1.95",
+  "version" : "2017-09-30",
+  "instanceType" : "t2.nano",
+  "billingProducts" : null,
+  "instanceId" : "i-03d629d19cb30deec",
+  "accountId" : "568333322432",
+  "availabilityZone" : "ap-southeast-2a",
+  "kernelId" : null,
+  "ramdiskId" : null,
+  "architecture" : "x86_64",
+  "imageId" : "ami-0aff30363d302d23a",
+  "pendingTime" : "2018-12-03T03:02:58Z",
+  "region" : "ap-southeast-2"
+}'''
+    return http_get('http://169.254.169.254/latest/dynamic/instance-identity/document')
+
+def http_pkcs7():
+    return \
+'''
+MIAGCSqGSIb3DQEHAqCAMIACAQExCzAJBgUrDgMCGgUAMIAGCSqGSIb3DQEHAaCAJIAEggHiewog
+ICJkZXZwYXlQcm9kdWN0Q29kZXMiIDogbnVsbCwKICAibWFya2V0cGxhY2VQcm9kdWN0Q29kZXMi
+IDogbnVsbCwKICAicHJpdmF0ZUlwIiA6ICIxMC4wLjEuOTUiLAogICJ2ZXJzaW9uIiA6ICIyMDE3
+LTA5LTMwIiwKICAiaW5zdGFuY2VUeXBlIiA6ICJ0Mi5uYW5vIiwKICAiYmlsbGluZ1Byb2R1Y3Rz
+IiA6IG51bGwsCiAgImluc3RhbmNlSWQiIDogImktMDNkNjI5ZDE5Y2IzMGRlZWMiLAogICJhY2Nv
+dW50SWQiIDogIjU2ODMzMzMyMjQzMiIsCiAgImF2YWlsYWJpbGl0eVpvbmUiIDogImFwLXNvdXRo
+ZWFzdC0yYSIsCiAgImtlcm5lbElkIiA6IG51bGwsCiAgInJhbWRpc2tJZCIgOiBudWxsLAogICJh
+cmNoaXRlY3R1cmUiIDogIng4Nl82NCIsCiAgImltYWdlSWQiIDogImFtaS0wYWZmMzAzNjNkMzAy
+ZDIzYSIsCiAgInBlbmRpbmdUaW1lIiA6ICIyMDE4LTEyLTAzVDAzOjAyOjU4WiIsCiAgInJlZ2lv
+biIgOiAiYXAtc291dGhlYXN0LTIiCn0AAAAAAAAxggEXMIIBEwIBATBpMFwxCzAJBgNVBAYTAlVT
+MRkwFwYDVQQIExBXYXNoaW5ndG9uIFN0YXRlMRAwDgYDVQQHEwdTZWF0dGxlMSAwHgYDVQQKExdB
+bWF6b24gV2ViIFNlcnZpY2VzIExMQwIJAJa6SNnlXhpnMAkGBSsOAwIaBQCgXTAYBgkqhkiG9w0B
+CQMxCwYJKoZIhvcNAQcBMBwGCSqGSIb3DQEJBTEPFw0xODEyMDMwMzAzMDZaMCMGCSqGSIb3DQEJ
+BDEWBBQ2fwDAlRfkdKZ0jjcL6Gy5nBoGnjAJBgcqhkjOOAQDBC4wLAIUfKtLotTzt7UtlgADpUIB
+CDG6insCFHLu1fQwkUgR2WiYP7kTIZPInslJAAAAAAAA
+-----END PKCS7-----
+'''
+    return http_get('http://169.254.169.254/latest/dynamic/instance-identity/pkcs7')
 
 def http_poll(url, token):
-    headers = {'Authorization': f'Bearer {token}'}
-    request = urllib.request.Request(f'{url}/poll', headers=headers)
-    with urllib.request.urlopen(request) as r:
-        status = r.getcode()
-        if status != 200:
-            raise Exception(f'poll returned unexpected status code {status}')
-        return json.loads(r.read())
+    return json.loads(http_get(url='{}/poll'.format(url), headers={'Authorization': 'Bearer {}'.format(token)}))
 
 def http_task(url, token, task, state):
-    headers = {'Authorization': f'Bearer {token}'}
-    request = urllib.request.Request(f'{url}/task/{task}/{state}', headers=headers)
-    with urllib.request.urlopen(request) as r:
-        status = r.getcode()
-        if status != 200:
-            raise Exception(f'task returned unexpected status code {status}')
+    http_get(url='{}/task/{}/{}'.format(url, task, state), headers={'Authorization': 'Bearer {}'.format(token)})
 
-def enrol(url, key):
-    logging.info('event=enrol url=%s', url)
-    account, instance = '11111111', 'aaaaaaaa' # todo: get ec2 metadata
-    response = http_enrol(url, key, account, instance)
-    if not os.path.exists(path):
-        os.makedirs(path)
-        os.chmod(path, 0o700)
-        os.makedirs(path + '/tasks')
-        os.makedirs(path + '/artefacts')
-    with open(path + '/url', 'w') as f:
-        f.write(url)
-    with open(path + '/token', 'w') as f:
-        f.write(response['access_token'])
+def get_token():
+    data = '{}\n-----BEGIN PKCS7-----\n{}\n-----END PKCS7-----'.format(http_document(), http_pkcs7())
+    return base64.b64encode(data.encode('utf-8')).decode('utf-8')
 
 def memory():
-    timestamp = int(time.time())
-    subprocess.check_call(['/usr/bin/local/linpmem-2.1.post4', '-o', f'{path}/artefacts/memory.{timestamp}.aff4r'], stdout=subprocess.PIPE)
+    filename = '{}/artefacts/memory.{}.aff4r'.format(path, int(time.time()))
+    subprocess.check_call(['/usr/bin/local/linpmem-2.1.post4', '-o', filename], stdout=subprocess.PIPE)
 
 def monitor(task_queue, url, token):
     while True:
@@ -82,46 +103,37 @@ def monitor(task_queue, url, token):
 def schedule(task_queue, task):
     task_path = '{}/tasks/{}'.format(path, task['id'])
     if not os.path.exists(task_path):
-        logging.info('event=schedule task=%s', task['id'])
         with open(task_path, 'w') as f:
             f.write(json.dumps(task))
-        task_queue.put(task)
+        if task['expires'] < int(time.time()):
+            logging.info('event=schedule task=%s', task['id'])
+            task_queue.put(task)
+        else:
+            logging.info('event=expired task=%s', task['id'])
 
-def poll():
-    if not os.path.exists(path):
-        logging.error('Agent has not been enrolled.')
-        return
-    with open(f'{path}/url', 'r') as f:
-        url = f.read()
-    with open(f'{path}/token', 'r') as f:
-        token = f.read()
-    task_queue = queue.Queue()
-    thread = threading.Thread(target=monitor, args=(task_queue, url, token))
-    thread.daemon = True
-    thread.start()
-    while True:
-        try:
-            logging.debug('event=poll url=%s', url)
-            for task in http_poll(url, token)['tasks']:
-                schedule(task_queue, task)
-            time.sleep(10)
-        except Exception:
-            logging.exception('poll')
+def poll(task_queue, url, token):
+    try:
+        logging.debug('event=poll url=%s', url)
+        for task in http_poll(url, token)['tasks']:
+            schedule(task_queue, task)
+    except Exception:
+        logging.exception('poll')
+    time.sleep(10)
 
 def main():
     try:
-        parser = argparse.ArgumentParser()
-        parser.add_argument('--url')
-        parser.add_argument('--key')
-        args = parser.parse_args()
-        if args.url and args.key:
-            enrol(args.url, args.key)
-        else:
-            poll()
+        with open('{}/url'.format(path), 'r') as f:
+            url = f.read().strip()
+        token = get_token()
+        token = 'x'
+        task_queue = queue.Queue()
+        thread = threading.Thread(target=monitor, args=(task_queue, url, token))
+        thread.daemon = True
+        thread.start()
+        while True:
+            poll(task_queue, url, token)
     except Exception:
         logging.exception('main')
-    except KeyboardInterrupt:
-        sys.stdout.flush()
 
 if __name__ == '__main__':
     main()
--- agent/install.sh
+++ agent/install.sh
@@ -1,10 +1,18 @@
 #!/usr/bin/env bash
 
-wget -O /usr/local/sbin/linpmem-2.1.post4 https://github.com/google/rekall/releases/download/v1.5.1/linpmem-2.1.post4
-chmod 700 /usr/local/sbin/linpmem-2.1.post4
+set -eux
+
+mkdir -p /usr/local/etc/acquired
+mkdir -p /usr/local/etc/acquired/tasks
+mkdir -p /usr/local/etc/acquired/artefacts
+echo "http://localhost:4444" > /usr/local/etc/acquired/url
+chmod 700 /usr/local/etc/acquired
 
 cp -f agent/acquired.py /usr/local/sbin
 chmod 700 /usr/local/sbin/acquired.py
 
 cp -f agent/acquired.service /etc/systemd/system/
 chmod 700 /etc/systemd/system/acquired.service
+
+wget -O /usr/local/sbin/linpmem-2.1.post4 https://github.com/google/rekall/releases/download/v1.5.1/linpmem-2.1.post4
+chmod 700 /usr/local/sbin/linpmem-2.1.post4
